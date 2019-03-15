38d3064fcc9404f77cf3c67e7da0c6c667f34ee7
980venkat <980venkat@gmail.com>
2019-03-15 14:31:39 -0400

    Create add_tags_ec2.sh

---
+++ tags/add_tags_ec2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for region in `aws ec2 describe-regions --output text | cut -f3`
+do
+    echo -e "\nListing Instances in region:'$region'..."
+
+    aws ec2 describe-instances --region $region   --query 'Reservations[*].Instances[*].{ID:InstanceId,LaunchTime:LaunchTime,Root:RootDeviceName,Type:InstanceType,Platform:Platform}' > $region-j.json
+
+    jq -c '.[][]' $region-j.json | while read i;
+    do
+       iid=$(jq -r '.ID' <<< "$i");
+       aws ec2 create-tags --resources $iid --tags Key=cost,Value=test
+    done
+
+done
