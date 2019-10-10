74d61d8615f3a24abfb72d4c2bb9d9c59745b49c
980venkat <980venkat@gmail.com>
2019-10-10 12:25:58 -0400

    Update sundar.py

--- sundar.py
+++ sundar.py
@@ -20,31 +20,8 @@ for index, row in df.iterrows():
       r3=-1
       print('Invalid -- ',row['TYPE'], row['REGION'], row['Platform'])
 
-    try:
-      r1 = ec2_offer.reserved_hourly(
-           row['TYPE'],
-           operating_system=row['Platform'],
-           lease_contract_length='1yr',
-           purchase_option='No Upfront',
-           region= row['REGION']
-           )
-    except :
-      r1=-1
-      print('Invalid -- ',row['TYPE'], row['REGION'], row['Platform'])
-
-    try:
-      od = ec2_offer.ondemand_hourly(
-           row['TYPE'],
-           operating_system=row['Platform'],
-           region= row['REGION']
-           )
-    except :
-      r1=-1
-      print('Invalid -- ',row['TYPE'], row['REGION'], row['Platform'])
-
-    print(index,'--',row['TYPE'], row['REGION'], row['Platform'],'--',r3,'--',r1,'--',od)
+    print(index,'--',row['TYPE'], row['REGION'], row['Platform'],'--',r3)
       #print row.items()
     df.set_value(index, '3Y-RI-NUF', r3)
-    df.set_value(index, '1Y-RI-NUF', r1)
-    df.set_value(index, 'OD Per Day', od)
-df.to_csv("test.csv", index=False)
+
+df.to_csv("out.csv", index=False)
